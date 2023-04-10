62d8735d199b537fb6bedcc408827876baeea099
mk <v.a.stekolschikov@gmail.com>
2023-04-10 17:17:09 +0300

    load img

--- src/home.tsx
+++ src/home.tsx
@@ -5,14 +5,17 @@ import {
   useNavigate
 } from "react-router-dom";
 import { storage } from "./firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL, listAll, StorageReference, } from "firebase/storage";
 
 
 const Home = () => {
 
   const [percent, setPercent] = useState(0);
-  const [file, setFile] = useState("");
+  const [refreshImgs, setRefreshImgs] = useState(0);
+  const [imgs, setImgs] = useState<string[]>([])
   let navigate = useNavigate();
+
+  const listRef = ref(storage, 'files');
 
   useEffect(() => {
     let authToken = sessionStorage.getItem('Auth Token')
@@ -43,17 +46,34 @@ const Home = () => {
           // download url
           getDownloadURL(uploadTask.snapshot.ref).then((url) => {
             console.log(url);
+
+            setRefreshImgs(refreshImgs + 1)
           });
         }
       );
     }
   }
 
+  useEffect(() => {
+
+    listAll(listRef)
+      .then(async (res) => {
+        const urls = await Promise.all(res.items.map(img => getDownloadURL(img)))
+        setImgs(urls)
+      }).catch((error) => {
+        console.log('error', error);
+      });
+  }, [refreshImgs])
+
   return <>
     Home
 
     <input type="file" accept="image/*" onChange={handleChange} />
     <p>{percent} "% done"</p>
+    <div style={{ padding: "20px" }}>
+      {imgs.map(img => <img src={img} style={{ width: "250px", display: "block" }} />)}
+
+    </div>
   </>
 
 }
